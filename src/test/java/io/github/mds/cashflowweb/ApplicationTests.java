48dcce53ddc38f089c38e036981ca856221705d2
AugustoRavazoli <augustoravazoli@gmail.com>
2024-03-04 07:59:58 -0300

    initial commit

---
+++ src/test/java/io/github/mds/cashflowweb/ApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.mds.cashflowweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
